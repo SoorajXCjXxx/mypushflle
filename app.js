eb9f38339b95bd0c0d7bf8582d4baf0c3bcfb301
SoorajXCjXxx <soorjkumarcj7@gmail.com>
2022-10-10 22:26:34 +0500

    huh

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const { Telegraf } = require('telegraf');
 require('dotenv').config();
 const axios = require("axios");
 const bot = new Telegraf(process.env.api);
+var http = require('http');
+let port = process.env.PORT|| 8000;
+
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World!');
+}).listen(port);
 // let words = bot.hears('s');
 bot.start((ctx) => ctx.reply('Enter I am Richard'));
 bot.help((ctx) => ctx.reply('Send me a sticker'));
