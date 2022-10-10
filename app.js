80e46a7fc2781e59707ddc428d846cd5fd256160
SoorajXCjXxx <soorjkumarcj7@gmail.com>
2022-10-10 22:01:11 +0500

    uploading on herohu

--- app.js
+++ app.js
@@ -3,21 +3,21 @@ require('dotenv').config();
 const axios = require("axios");
 const bot = new Telegraf(process.env.api);
 // let words = bot.hears('s');
-bot.start((ctx) => ctx.reply('Welcome'));
+bot.start((ctx) => ctx.reply('Enter I am Richard'));
 bot.help((ctx) => ctx.reply('Send me a sticker'));
 bot.on('sticker', (ctx) => ctx.reply('👍'));
 bot.hears('hi', (ctx) => ctx.reply('Hey there'));
 bot.hears('hello', (ctx) => ctx.reply('Hey there'));
 bot.hears('kitto', (ctx) => ctx.reply('Brother please add me in your gp'));
 bot.hears('k', (c) => c.reply('Brother please add me in your gp'));
-bot.on('message', (c)=> c.reply('fuck u '));
+bot.hears('I am Richard', (c) => c.reply('Bro how the war is going'));
+bot.hears('', (c) => c.reply('Bro how the war is going'));
+bot.on('message', (c)=> c.reply(' Thanks for chating with richard '));
 // bot.text('hello',(ctx)=>ctx.reply("hello brother"))
 
 bot.launch();
 
-// Enable graceful stop
+
 process.once('SIGINT', () => bot.stop('SIGINT'));
 process.once('SIGTERM', () => bot.stop('SIGTERM'));
-console.log(
-    
-);
+
