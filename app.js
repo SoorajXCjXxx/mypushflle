baccefe060b5e875a314013107ed2ef9eb90e311
SoorajXCjXxx <soorjkumarcj7@gmail.com>
2022-10-10 22:45:16 +0500

    ik

--- app.js
+++ app.js
@@ -10,21 +10,4 @@ http.createServer(function (req, res) {
   res.end('Hello World!');
 }).listen(port);
 // let words = bot.hears('s');
-bot.start((ctx) => ctx.reply('Enter I am Richard'));
-bot.help((ctx) => ctx.reply('Send me a sticker'));
-bot.on('sticker', (ctx) => ctx.reply('👍'));
-bot.hears('hi', (ctx) => ctx.reply('Hey there'));
-bot.hears('hello', (ctx) => ctx.reply('Hey there'));
-bot.hears('kitto', (ctx) => ctx.reply('Brother please add me in your gp'));
-bot.hears('k', (c) => c.reply('Brother please add me in your gp'));
-bot.hears('I am Richard', (c) => c.reply('Bro how the war is going'));
-// bot.hears('', (c) => c.reply('Bro how the war is going'));
-bot.on('message', (c)=> c.reply(' Thanks for chating with richard '));
-// bot.text('hello',(ctx)=>ctx.reply("hello brother"))
-
-bot.launch();
-
-
-process.once('SIGINT', () => bot.stop('SIGINT'));
-process.once('SIGTERM', () => bot.stop('SIGTERM'));
 
